8be0930449fcf8b20a82e589ca7c800d23f052d0
Nawapunth Manusitthipol <Nawa@dssbsoft.com>
2013-08-08 02:15:47 -0600

    Clean up Scheme.

--- DirectCryptography/source/dssb/cryptography/Scheme.java
+++ DirectCryptography/source/dssb/cryptography/Scheme.java
@@ -1,8 +1,18 @@
 package dssb.cryptography;
 
 
+/**
+ * Cryptography scheme.
+ * 
+ * @author Nawapunth Manusitthipol <nawa@dssbsoft.com>
+ */
 public interface Scheme {
     
+    /**
+     * Creates and return a new cryptography builder.
+     * 
+     * @return the builder.
+     */
     public CryptographyBuilder createCryptographyBuilder();
     
 }
