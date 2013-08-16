c1550ce1345b4589e602969e02d97fda77386987
Nawapunth Manusitthipol <Nawa@dssbsoft.com>
2013-08-16 09:17:56 -0600

    Satisfy checkstyle.

--- DirectCryptography/test/dssb/cryptography/TestSerializableType.java
+++ DirectCryptography/test/dssb/cryptography/TestSerializableType.java
@@ -9,40 +9,52 @@ import dssb.cryptography.datatypes.ArrayOfBytesType;
 import dssb.cryptography.datatypes.SerializableType;
 import dssb.cryptography.datatypes.SerializablesType;
 
+/**
+ * This test ensure serializable type works.
+ * 
+ * @author Nawapunth Manusitthipol <nawa@dssbsoft.com>
+ */
 public class TestSerializableType {
     
+    /** Try to used ut to store an int. */
     @Test
     public void testInt() {
         final Integer data = 10;
         final SerializableType<Integer> type = new SerializableType<Integer>(Integer.class);
         final byte[] bytes = type.toBytes(data);
-        Assert.assertEquals(Integer.valueOf(10), (Integer)type.fromBytes(bytes));
+        Assert.assertEquals(Integer.valueOf(data), (Integer) type.fromBytes(bytes));
     }
     
+    /** Try to used it to store a string. */
     @Test
     public void testString() {
         final String data = "String";
         final SerializableType<String> type = new SerializableType<String>(String.class);
         final byte[] bytes = type.toBytes(data);
-        Assert.assertEquals("String", (String)type.fromBytes(bytes));
+        Assert.assertEquals("String", (String) type.fromBytes(bytes));
     }
     
+    /** Try to used it to store a byte array. */
     @Test
     public void testBytes() {
         final byte[] data = "String".getBytes();
         final SerializableType<byte[]> type = new SerializableType<byte[]>(byte[].class);
         final byte[] bytes = type.toBytes(data);
-        Assert.assertEquals(Arrays.toString("String".getBytes()), Arrays.toString((byte[])type.fromBytes(bytes)));
+        Assert.assertEquals(Arrays.toString("String".getBytes()), Arrays.toString((byte[]) type.fromBytes(bytes)));
     }
     
+    /** Try to used it to store a bytes array. */
     @Test
     public void testBytesArray() {
-        final byte[] data1 = ("" + (char)65).getBytes();
-        final byte[] data2 = ("" + (char)66).getBytes();
+        final byte a = 'A';
+        final byte b = 'B';
+        final byte[] data1 = ("" + a).getBytes();
+        final byte[] data2 = ("" + b).getBytes();
         final byte[] data = ArrayOfBytesType.TYPE.toBytes(data1, data2);
         Assert.assertEquals("[0, 0, 0, 2, 0, 0, 0, 1, 65, 0, 0, 0, 1, 66]", Arrays.toString(data));
     }
     
+    /** Try to used it to store a serializable array. */
     @Test
     public void testSerializables() {
         final Integer data1 = 10;
@@ -51,7 +63,7 @@ public class TestSerializableType {
         final byte[] bytes = type.toBytes(data1, data2);
         System.out.println(Arrays.toString(bytes));
         System.out.println(Arrays.toString(type.fromBytes(bytes)));
-        Assert.assertEquals(Arrays.toString(new Integer[] { 10, 20 }), Arrays.toString(type.fromBytes(bytes)));
+        Assert.assertEquals(Arrays.toString(new Integer[] {data1, data2}), Arrays.toString(type.fromBytes(bytes)));
     }
     
 }
